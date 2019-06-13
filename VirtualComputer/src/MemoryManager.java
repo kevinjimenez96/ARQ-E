ca90372d83f0e6c474c12254e80bb1d0638eda44
Jefferson Alvarez <jeffalvarez08@gmail.com>
2019-06-13 16:06:54 -0600

    MemoryManager added

---
+++ VirtualComputer/src/MemoryManager.java
@@ -0,0 +1,21 @@
+import java.util.BitSet;
+
+public class MemoryManager {
+
+    private MainMemory mainMemory = new MainMemory();
+
+    /**
+     *
+     */
+    MemoryManager(){
+        System.out.println("Constructor of Memory Manager");
+    }
+
+    /**
+     *
+     */
+    public void allocate(BitSet address){
+
+    }
+
+}
--- VirtualComputer/src/ProgramLoader.java
+++ VirtualComputer/src/ProgramLoader.java
@@ -1,10 +1,41 @@
+import java.io.File;
+import java.util.BitSet;
+
 public class ProgramLoader {
+
+    private MemoryManager memoryManager = new MemoryManager();
 
     /**
      *
      */
     ProgramLoader (){
         System.out.println("Constructor of ProgramLoader class");
+    }
+
+    /**
+     *
+     * @param file
+     * @return
+     */
+    public BitSet loadProgram(File file){
+        System.out.println("Load program of Program Loader");
+        return  new BitSet();
+    }
+
+    /**
+     *
+     * @param instruction
+     */
+    private void loadInstruction(BitSet instruction){
+        System.out.println("Load instruction of Program Loader");
+    }
+
+    /**
+     *
+     * @param data
+     */
+    private void loadData(BitSet data){
+        System.out.println("Load data of Load Program");
     }
 
 }
