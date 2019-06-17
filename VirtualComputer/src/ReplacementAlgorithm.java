d76487cacbe466ead5bc5347eeb057cec4f6aa7b
Jefferson Alvarez <jeffalvarez08@gmail.com>
2019-06-17 08:04:13 -0600

    Agrego los metodos de cacheblock, replacementAlg y Cache

--- VirtualComputer/src/ReplacementAlgorithm.java
+++ VirtualComputer/src/ReplacementAlgorithm.java
@@ -1,7 +1,19 @@
+import java.util.BitSet;
+import java.util.Queue;
+
 public class ReplacementAlgorithm {
 
     ReplacementAlgorithm(){
         System.out.println("Constructor of Replacement Algorithm");
+    }
+
+    /**
+     *
+     * @param cola
+     * @param datos
+     */
+    public void replace(Queue<Integer> cola, BitSet datos){
+
     }
 
 }
