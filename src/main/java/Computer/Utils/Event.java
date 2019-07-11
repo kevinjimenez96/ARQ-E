5f196e2575d9638182f5a4d9e00beadc38e2bb5b
aflaster11 <a.flaster@gmail.com>
2019-07-11 16:32:59 -0600

    Comparable Event

--- src/main/java/Computer/Utils/Event.java
+++ src/main/java/Computer/Utils/Event.java
@@ -1,6 +1,8 @@
 package Computer.Utils;
 
-public class Event {
+import java.util.Objects;
+
+public class Event implements Comparable<Event> {
 
     private EventType type;
     private long executionStart;
@@ -28,5 +30,34 @@ public class Event {
 
     public void setExecutionDuration(int executionDuration) {
         this.executionDuration = executionDuration;
+    }
+
+    @Override
+    public int compareTo(Event event) {
+        int cmp = 0;
+        if (this.executionStart + this.executionDuration < event.getExecutionStart() + event.getExecutionDuration()) {
+            cmp = -1;
+        } else {
+            if (this.executionStart + this.executionDuration > event.getExecutionStart() + event.getExecutionDuration()) {
+                cmp = 1;
+            }
+        }
+        return cmp;    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return executionStart == event.executionStart &&
+                executionDuration == event.executionDuration &&
+                type == event.type;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(type, executionStart, executionDuration);
     }
 }
