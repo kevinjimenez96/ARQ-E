328be3ddb3e716ed347d06a1abad440f9da2d8f7
aflaster11 <a.flaster@gmail.com>
2019-07-10 18:00:43 -0600

    Switched to mvn project and added RxBus

---
+++ src/main/java/Computer/RxBus.java
@@ -0,0 +1,33 @@
+package Computer;
+
+import rx.Subscription;
+import rx.functions.Action1;
+import rx.subjects.PublishSubject;
+import rx.subjects.SerializedSubject;
+import rx.subjects.Subject;
+
+
+public class RxBus {
+
+    private static final RxBus INSTANCE = new RxBus();
+
+    private final Subject<Object, Object> mBusSubject = new SerializedSubject<>(PublishSubject.create());
+
+    public static RxBus getInstance() {
+        return INSTANCE;
+    }
+
+    private RxBus() {
+    }
+
+    public <T> Subscription register(final Class<T> eventClass, Action1<T> onNext) {
+        return mBusSubject
+                .filter(event -> event.getClass().equals(eventClass))
+                .map(obj -> (T) obj)
+                .subscribe(onNext);
+    }
+
+    public void post(Object event) {
+        mBusSubject.onNext(event);
+    }
+}
